b51940614dc10c2079cec70edaff771588f64942
itfibonacci <levon.tumanyan@outlook.com>
2024-02-22 11:32:01 -0500

    More tests

--- tests/test_initialize_allocator.c
+++ tests/test_initialize_allocator.c
@@ -15,6 +15,9 @@ struct Block *create_initial_pool(size_t size);
 void test_initial_pool_not_null(struct Block *first_pool);
 void test_initial_pool_free(struct Block *first_pool);
 void test_initial_pool_size(struct Block *first_pool);
+void test_initial_pool_prev_null(struct Block *first_pool);
+void test_initial_pool_next_null(struct Block *first_pool);
+void test_singleton_behaviour();
 
 int main() {
 	size_t size = 1024;
@@ -22,6 +25,9 @@ int main() {
 	test_initial_pool_not_null(initial_pool);
 	test_initial_pool_free(initial_pool);
 	test_initial_pool_size(initial_pool);
+	test_initial_pool_prev_null(initial_pool);
+	test_initial_pool_next_null(initial_pool);
+	test_singleton_behaviour();
 	return 0;
 }
 
@@ -33,18 +39,43 @@ struct Block *create_initial_pool(size_t size) {
 
 void test_initial_pool_not_null(struct Block *first_pool) {
 	printf("Checking if the initial pool is not null. ❓\n");
-	ASSERT(first_pool != NULL, "The initial pool is null.");
+	ASSERT(first_pool != NULL, "The initial pool is null. 😨");
 	printf("The initial pool is not null. ✅\n");
 }
 
 void test_initial_pool_free(struct Block *first_pool) {
 	printf("Checking if the initial pool is marked as free. ❓\n");
-	ASSERT(first_pool->free == 1, "The initial pool is not free.");
+	ASSERT(first_pool->free == 1, "The initial pool is not free. 😨");
 	printf("The initial pool is marked as free. ✅\n");
 }
 
 void test_initial_pool_size(struct Block *first_pool) {
 	printf("Checking if the initial pool's size is correct. ❓\n");
-	ASSERT(first_pool->size == 1024, "The initial pool's size is not correct.");
+	ASSERT(first_pool->size == 1024, "The initial pool's size is not correct. 😨");
 	printf("The initial pool's size is correct as expected. (%zu bytes.) ✅\n", first_pool->size);
+}
+
+void test_initial_pool_prev_null(struct Block *first_pool) {
+	printf("Checking if the initial pool's prev is null. ❓\n");
+	ASSERT(first_pool->prev == NULL, "The initial pool's prev is not null. 😨");
+	printf("The initial pool's prev is null. ✅\n");
+}
+
+void test_initial_pool_next_null(struct Block *first_pool) {
+	printf("Checking if the initial pool's next is null. ❓\n");
+	ASSERT(first_pool->next == NULL, "The initial pool's next is not null. 😨");
+	printf("The initial pool's next is null. ✅\n");
+}
+
+// Test for singleton behavior
+void test_singleton_behaviour() {
+	size_t size1 = 1024;
+	struct Block *initial_pool1 = initialize_allocator(size1);
+	
+	size_t size2 = 2048;
+	struct Block *initial_pool2 = initialize_allocator(size2);
+
+	printf("Checking for the singleton behaviour of initialize allocator. ❓\n");
+	ASSERT(initial_pool1 == initial_pool2, "Two pools returned different pointers. 😨");
+	printf("Singleton behaviour confirmed. Pointers: %p %p ✅\n", initial_pool1, initial_pool2);
 }
