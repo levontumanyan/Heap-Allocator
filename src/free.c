d4e2a67b0474d2516e057d60a023d077b9fb4176
itfibonacci <levon.tumanyan@outlook.com>
2024-02-28 12:50:45 -0500

    free + merge blocks for defragmentation

--- src/free.c
+++ src/free.c
@@ -2,6 +2,7 @@
 
 /* 
 return 0 if success and non-zero otherwise
+- what about actually releasing memory from the process not just marking the block free***
 - at some point can add checks to make sure the passed pointer does point to a block
 - make sure to coalesce free blocks next to each other - to reduce fragmentation
 */
@@ -21,18 +22,51 @@ unsigned char my_free(void *ptr) {
 }
 
 unsigned char merge_blocks(Block* block) {
-	/* thinking about doing a for loop, at first to the "left" of the passed block.
+	if (block == NULL) {
+		return 1;
+	}
+	/* thinking about doing a while loop, at first to the "left" of the passed block.
 	we go to the previous block check if it is free, if it is we merge them together
 	when we merge we have to make sure to remove the info about the merging block */
 
 	// let's keep track of the size of the blocks counted so far
-	size_t new_size;
+	size_t new_size = 0;
+	
 	// first let's go back until we see a non-free block
+	Block *last_visited_block = block;
 	Block *current_block = block;
-	while(current_block != NULL || current_block->free == 0) {
+	while(current_block != NULL && current_block->free == 1) {
 		new_size += current_block->size;
+		last_visited_block = current_block;
 		current_block = current_block->prev;
 	}
+	/* when we come out of the while loop current_block will either be NULL or not free. in the case when current_block is NULL means that the next block of current block should become the new beginning block. */
 
-	/* when we come out of the for loop current_block will either be NULL or not free. in the case when current_block is NULL means that the next block of current block should become the new beginning block.  */
+	// first block to start combining from
+	Block *beginning_block = last_visited_block;
+	
+	// let's go back to the block and start going to the right and finding free blocks next to each other to combine
+	current_block = block;
+	last_visited_block = block;
+	while(current_block != NULL && current_block->free == 1) {
+		new_size += current_block->size;
+		last_visited_block = current_block;
+		current_block = current_block->next;
+	}
+	// this last_block is the last one that we can combine.
+	Block *last_block = last_visited_block;
+
+	Block *combined_free_block = beginning_block;
+	combined_free_block->free = 1;
+	combined_free_block->prev = beginning_block->prev;
+	combined_free_block->next = last_block->next;
+	combined_free_block->size = new_size;
+
+	// update the blocks before combined_block and after it.
+	if (combined_free_block->prev != NULL) {
+		combined_free_block->prev->next = combined_free_block;
+	}
+	if (combined_free_block->next != NULL) {
+		combined_free_block->next->prev = combined_free_block;
+	}
 }
